22ee2370b71dec7f8f721007d0168a307c9542dd
susel <suselrd@gmail.com>
2013-07-11 17:13:34 -0500

    settings

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -38,8 +38,8 @@ else:
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'cubicate',                                 # Or path to database file if using sqlite3.
             'USER': 'postgres',                                 # Not used with sqlite3.
-            'PASSWORD': 'postgres',                             # Not used with sqlite3.
-            'HOST': '127.0.0.1',                                # Set to empty string for localhost. Not used with sqlite3.
+            'PASSWORD': 'Adminp455w0rd2013',                             # Not used with sqlite3.
+            'HOST': '192.168.137.1',                                # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '5432',                                     # Set to empty string for default. Not used with sqlite3.
         }
     }
